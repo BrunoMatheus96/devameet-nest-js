ffbd41aa974dfd723ac765e8ff9d6f04f4040c7f
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2023-12-28 09:08:26 -0300

    Aula parte 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config'; 
 import { AuthModule } from './auth/auth.module'; 
 import { UserModule } from './user/user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
 
 //Decorator do estilo módulo
 /*Módulo é um agrupamento de arquivo que fechamos como se fosse um micro serviço. Então módulo é uma parte do código
@@ -15,6 +17,8 @@ que funciona de forma autonoma (pode ter entrada e aída) mas funciona sozinho.*
    UserModule  
   ], 
   controllers: [], 
-  providers: [], 
+  providers: [
+    {provide: APP_GUARD, useClass: JwtAuthGuard}
+  ], 
  }) 
  export class AppModule {}
